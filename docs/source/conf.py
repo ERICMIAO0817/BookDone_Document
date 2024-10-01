08a9708307cd7cd7a7c1322b7e0caafabc0ca439
ERICMIAO0817 <1838040569@qq.com>
2024-10-01 13:16:16 +0800

    name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'BookDone'
+copyright = '2024, BookDone-Team'
+author = 'Eric'
 
 release = '0.1'
 version = '0.1.0'
